797b16857ac3ec1fb87e371946d19f6015f18ef7
Sorindy <hot.sorindy@gmail.com>
2017-10-03 19:10:48 +0700

    done some of mine

---
+++ Patient/PatientManagement/PatientManagement/Class/ServiceCategory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital_Entity_Framework;
+
+namespace PatientManagement.Class
+{
+    class ServiceCategory
+    {
+        HospitalDbContext _db=new HospitalDbContext();
+
+        public void Insert(string serviceId, string categoryId)
+        {
+            var insert=new Hospital_Entity_Framework.ServiceCategory(){ServiceId = serviceId,CategoryId = categoryId};
+            _db.ServiceCategories.Add(insert);
+        }
+
+        public void Delete(string serviceId)
+        {
+            var delete = _db.ServiceCategories.Single(v => v.ServiceId == serviceId);
+
+            _db.ServiceCategories.Remove(delete);
+            _db.SaveChanges();
+        }
+    }
+}
